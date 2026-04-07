7e0b71269097c4c1515e5ad2b83a5d0c8e84a5c8
Shubham <sp5535258@gmail.com>
2026-04-07 12:33:45 +0530

    Fix auth login pages

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import LoginPage from "@/components/auth/LoginPage";
-=======
-"use client";
-import LoginPage from "@/components/auth/loginpage";
->>>>>>> dev
 
 export default function Page() {
   return <LoginPage />;
